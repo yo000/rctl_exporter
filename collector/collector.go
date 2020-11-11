dd2356c89cac7ca72296e2f591c4cb06996ba4df
yo <johan@nosd.in>
2020-11-11 14:28:10 +0100

    Moving collector logic to collector module

--- collector/collector.go
+++ collector/collector.go
@@ -10,19 +10,28 @@ import (
 	//	"fmt"
 	//	"strings"
 
+	"strings"
+
+	// local import, see go.mod
+	"coincoin.org/rctl_exporter"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 type Collector struct {
-	up *prometheus.Desc
+	resrces []*Resource
+	log     *logrus.Logger
+	up      *prometheus.Desc
 	// ... declare some more descriptors here ...
 }
 
 // instantiate a collector object
-func New() *Collector {
+func New(resrc []*Resource, log *logrus.Logger) *Collector {
 	return &Collector{
 
-		up: prometheus.NewDesc("rctl_up", "Whether scraping rctl's metrics was successful", []string{"collectFilter"}, nil),
+		//up: prometheus.NewDesc("rctl_up", "Whether scraping rctl's metrics was successful", []string{"collectFilter"}, nil),
+		up:      prometheus.NewDesc("rctl_up", "Whether scraping rctl's metrics was successful", []string{"pid", "cmdline"}, nil),
+		log:     log,
+		resrces: resrc,
 
 		// ... initialize rest of the descriptors ...
 		// ... do other initialization ...
@@ -36,6 +45,30 @@ func (c *Collector) Describe(ch chan<- *prometheus.Desc) {
 	// ... describe other metrics ...
 }
 
+func collectFromResourceStruct(resrces []*Resource) {
+	// 1. Describe metrics by
+	//		- building names with prometheus.BuildFQName
+	//		- Declare them with prometheus.NewDesc(fqname, help, variablelabels, constlabels)
+	// 2. Send metrics value with MustNewConstMetric(desc, type, value, labels, labels,...)
+
+	// Example of metric names :
+	// rctl_usage_process_cputime{pid="713", cmdline="/usr/local/sbin/libvirtd --daemon --pid-file=/var/run/libvirtd.pid"}
+	// rctl_usage_user_cputime{user="yo"}
+	// rctl_usage_loginclass{class="daemon"}
+	// rctl_usage_jail{jid="120", name="dovecot"}
+
+	for _, resrcObj := range resrces {
+		if resrcObj.resrctype == "process" {
+			for _, resrc := range resrcObj.rawresources {
+				s := strings.Split(resrc, "=")
+				d := prometheus.NewDesc("rctl_usage_process_"+s[0], "man rctl", []string{"pid", "cmdline"}, nil)
+				ch <- prometheus.MustNewConstMetric(d, prometheus.UntypedValue, s[1], []string{resrc.GetResourceID(), resrc.GetProcessCommandLine()})
+			}
+		} else if resrcObj.resrctype == "user" {
+		}
+	}
+}
+
 // Collect - called to get the metric values
 func (c *Collector) Collect(ch chan<- prometheus.Metric) {
 	//if stats, err := c.client.GetServiceBusStats(); err != nil {
@@ -45,6 +78,8 @@ func (c *Collector) Collect(ch chan<- prometheus.Metric) {
 	//	} else {
 	//		// client call succeeded, set the up metric value to 1
 	ch <- prometheus.MustNewConstMetric(c.up, prometheus.GaugeValue, 1, "collectFilterGoesHere")
+
+	collectFromResourceStruct(resrces)
 
 	// ... collect other metrics ...
 	//	}
